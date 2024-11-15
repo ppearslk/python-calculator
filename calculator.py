f19e0dd4cd10060ce277c35031e1272ace67a632
ppearslk <pearsaligupt@gmail.com>
2024-11-15 06:01:22 +0000

    Add the test cases

--- calculator.py
+++ calculator.py
@@ -7,22 +7,39 @@ class Calculator:
 
     def multiply(self, a, b):
         result = 0
+        # ถ้าเป็นลบทั้งคู่จะเป็น Flase
+        negative_result = (a < 0) ^ (b < 0)
+        # ทำให้เป็นค่าสัมบูรณ์เพื่อง่ายต่อการคำนวณ
+        a, b = abs(a), abs(b)
+        # ทำการบวกค่าเรื่อยๆจำนวนครั้งเท่ากับ b 
         for i in range(b):
             x = a 
             result = self.add(result, x )
+        # ทำการเปลี่ยนเครื่องหมาย result เมื่อ negative_result เมื่อมีค่าเป้น True 
+        if negative_result:
+            result = -result
         return result
 
     def divide(self, a, b):
         result = 0
+        negative_result_divide = (a < 0) ^ (b < 0) 
+        a, b = abs(a), abs(b)
         while a >= b:
             a = self.subtract(a , b)
             result += 1
+        if negative_result_divide:
+            result = -result
         return result
     
     def modulo(self, a, b):
+        negative_result_modulo  = (a < 0) ^ (b < 0) 
+        a, b = abs(a), abs(b)
         while b < a:
             a = a - b
+        if negative_result_modulo :
+            a = -a
         return a
+
 
 # Example usage:
 if __name__ == "__main__":
@@ -30,6 +47,6 @@ if __name__ == "__main__":
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(-4 , 3))
-    print("Example: division: ", calc.divide(4, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: multiplication: ", calc.multiply(-4 , -20))
+    print("Example: division: ", calc.divide(-14, 2))
+    print("Example: modulo: ", calc.modulo(-35, 3))
--- test_calculator.py
+++ test_calculator.py
@@ -22,27 +22,25 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(2,1),2)
     def test2_multiply_nagative(self):
         self.assertEqual(self.calc.multiply(-10,10),-100)
-
-    # def test3_multiply_nagative_and_nagative(self):
-    #     self.assertEqual(self.calc.multiply(-10,-5),50)
+    def test3_multiply_nagative_and_nagative(self):
+        self.assertEqual(self.calc.multiply(-10,-5),50)
 
 # divide ---------------------------------------------------------------------
     def test1_divide(self):
         self.assertEqual(self.calc.divide(4,2),2)
-
-    # def test2_divide_nagative(self):
-    #     self.assertEqual(self.calc.divide(-155,5),-31)
-    # def test3_divide_nagative_and_nagative
-    #     self.assertEqual(self.calc.divide(-155,-5),31)
+    def test2_divide_nagative(self):
+        self.assertEqual(self.calc.divide(-155,5),-31)
+    def test3_divide_nagative_and_nagative(self):
+        self.assertEqual(self.calc.divide(-155,-5),31)
 
 
 # modulo --------------------------------------------------------------------
     def test1_modulo(self):
         self.assertEqual(self.calc.modulo(21,2),1)
-    # def test2_modulo(self):
-    #     self.assertEqual(self.calc.modulo(-35,3),2)
+    def test2_modulo_nagative(self):
+        self.assertEqual(self.calc.modulo(-35,3),-2)
 
     # Add the following test methods to the TestCalculator class:
-
+    
 if __name__ == '__main__':
     unittest.main()
